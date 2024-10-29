922c0e26063130f7ee477730c65c795a1be3790e
gvinod0007 <gvinod0007@gmail.com>
2024-10-29 16:21:26 +0530

    new changes added

--- app.py
+++ app.py
@@ -3,9 +3,13 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 
-@app.route("/hello", methods=["GET"])
-def say_hello():
-    return jsonify({"msg": "Hello vinod"})
+# @app.route("/hello", methods=["GET"])
+# def say_hello():
+#     return jsonify({"msg": "Hello vinod"})
+
+@app.route("/<name>", methods=["GET"])
+def say_hello(name):
+    return jsonify({"msg": f"Hello {name}"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
